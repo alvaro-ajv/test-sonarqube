8776ba02a745c772089bcba182c5a177e6fee4cb
Alvaro Vargas Vargas <alvaro.jose.vargas.vargas@kyndryl.com>
2025-04-25 15:59:53 -0600

    Update app title in unit test to reflect version suffix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'test-sonarqube'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-sonarqube');
+    expect(app.title).toEqual('test-sonarqube-1');
   });
 
   it('should render title', () => {
