52ac73bd36d229bfbba8ad5b5816cfb078099101
Alvaro Vargas Vargas <alvaro.jose.vargas.vargas@kyndryl.com>
2025-04-28 15:05:05 -0600

    Update unit test to reflect updated app name in output message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-sonarqube-1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('test-sonarqube app is running!');
   });
 });
